0c1e66342d7cf6c0b8b2c9e82670ef31ae4a9f76
akashcoder02 <126484113+akashcoder02@users.noreply.github.com>
2026-04-01 15:38:32 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end("Hello CI Pipeline!");
+  res.end("Hello from Industry GitHub Actions!");
 });
 
 server.listen(3000, () => {
